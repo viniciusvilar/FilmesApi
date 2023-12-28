05211ab7459f0986fe6857a664416ed7a50cbacf
Vinícius Vilar <viniciusvillarma7x@gmail.com>
2023-12-28 15:31:36 -0300

    Adicionando restrições de obrigatoriedade

--- FilmesApi/FilmesApi/Models/Filme.cs
+++ FilmesApi/FilmesApi/Models/Filme.cs
@@ -10,6 +10,6 @@ public class Filme
     [MaxLength(50, ErrorMessage ="O tamanho do gênero não pode exceder 50 caracteres")]
     public string Genero { get; set; }
     [Required]
-    [Range(70, 600, ErrorMessage ="A duração deve ser entre 70 e 600 minutos")]
+    [Range(70, 600, ErrorMessage ="A duração deve ter entre 70 e 600 minutos")]
     public int Duracao { get; set; }
 }
